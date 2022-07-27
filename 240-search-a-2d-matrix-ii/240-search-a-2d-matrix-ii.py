c17679b956f588495aac24a6edd203bf518c4ff6
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-07-28 01:20:31 +0530

    Time: 263 ms (50.12%), Space: 20.6 MB (13.29%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
@@ -4,22 +4,25 @@ class Solution:
         n=len(matrix[0])
         if target<matrix[0][0] or target>matrix[m-1][n-1]:
             return False
-        a,b=0,0
-        for i in range(m):
-            if matrix[i][0]<=target:
-                a+=1
-            else:
+        t1,t2=0,0
+        for i in range(max(m,n)):
+            if i<n:
+                if matrix[0][i]<=target:
+                    b=i
+                else:
+                    t1=1
+            if i<m:
+                if matrix[i][0]<=target:
+                    a=i
+                else:
+                    t2=1
+            if t1>0 and t2>0:
                 break
-        for i in range(n):
-            if matrix[0][i]<=target:
-                b+=1
-            else:
-                break
-        for i in range(a):
+        for i in range(a+1):
             l=matrix[i]
             low=0
             mid=0
-            high=b-1
+            high=b
 
             while low<=high:
                 mid=(high+low)//2
