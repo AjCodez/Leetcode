5069b21cb913c39e890c46726c40120901edef29
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-07-29 03:31:23 +0530

    getSubSequence

---
+++ getSubSequence.py
@@ -0,0 +1,12 @@
+def getSS(s):
+    if s=='':
+        return ['']
+    cc=s[0]
+    ss=s[1:]
+    tres=getSS(ss)
+    res = []
+    for i in tres:
+        res.append(i)
+        res.append(cc+i)
+    return res
+print(getSS('abc'))
