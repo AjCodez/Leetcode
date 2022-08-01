929dac09bf86a9c5c91fc57ae93040bd404af10a
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-08-01 22:59:23 +0530

    Time: 35 ms (88.24%), Space: 13.7 MB (98.10%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,15 +1,8 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        if m==0 or n==0:
-            return 1
-        m-=1
-        n-=1
-        if m<n:
-            m,n=n,m
+        return self.comb(m+n-2,n-1)
+    def comb(self,a,b):
         res=1
-        i=1
-        for j in range(m+1,m+n+1):
-            res*=j
-            res/=i
-            i+=1
+        for i in range(b+1,a+1):
+            res = (res*i)/(i-b)
         return int(res)
