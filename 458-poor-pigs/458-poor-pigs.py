90dca3a17861a74adc65b7405d021dacaea28b7c
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-08-06 23:26:21 +0530

    Time: 46 ms (51.09%), Space: 13.9 MB (74.45%) - LeetHub

--- 458-poor-pigs/458-poor-pigs.py
+++ 458-poor-pigs/458-poor-pigs.py
@@ -1,6 +1,4 @@
+import math
 class Solution:
     def poorPigs(self, buckets: int, minutesToDie: int, minutesToTest: int) -> int:
-        pigs=0
-        while (minutesToTest/minutesToDie+1)**pigs<buckets:
-            pigs+=1
-        return pigs
+        return math.ceil(math.log(buckets)/math.log(minutesToTest//minutesToDie+1))
