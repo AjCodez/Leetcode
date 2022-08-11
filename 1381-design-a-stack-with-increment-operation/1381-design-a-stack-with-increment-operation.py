8a452fc070622b2c5418b7be3c6f2ab2a59add68
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-08-12 02:36:47 +0530

    Time: 206 ms (38.19%), Space: 15 MB (34.25%) - LeetHub

---
+++ 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.py
@@ -0,0 +1,34 @@
+class CustomStack:
+
+    def __init__(self, maxSize: int):
+        self.maxSize=maxSize
+        self.size=0
+        self.st=[]
+
+    def push(self, x: int) -> None:
+        if self.size!=self.maxSize:
+            self.st.append(x)
+            self.size+=1
+
+    def pop(self) -> int:
+        if self.st:
+            self.size-=1
+            return self.st.pop()
+        return -1
+
+    def increment(self, k: int, val: int) -> None:
+        if len(self.st)<=k:
+            for i in range(len(self.st)):
+                self.st[i]+=val
+        else:
+            ind=0
+            for i in range(k):
+                self.st[ind]+=val
+                ind+=1
+
+
+# Your CustomStack object will be instantiated and called as such:
+# obj = CustomStack(maxSize)
+# obj.push(x)
+# param_2 = obj.pop()
+# obj.increment(k,val)
