3d68fefa7940916a46f5d68d6cd943d5c7e8dfe5
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-08-14 02:30:47 +0530

    Time: 573 ms (59.63%), Space: 14.2 MB (38.75%) - LeetHub

--- 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.py
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.py
@@ -2,15 +2,22 @@ class Solution:
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
         ans=[]
         l=len(words[0])
+        dp={}
+        for i in words:
+            if i in dp:
+                dp[i]+=1
+            else:
+                dp[i]=1
         for i in range(len(s)):
             if len(s[i:])<(len(words)*l):
                 break
-            words2=list(words)
+            words2=dp.copy()
             j=i
             while s[i:i+l] in words2:
-                words2.remove(s[i:i+l])
-                i=i+l
+                words2[s[i:i+l]]-=1
+                if words2[s[i:i+l]]==0:
+                    words2.pop(s[i:i+l])
+                i+=l
             if not words2:
                 ans.append(j)
-                
         return ans
