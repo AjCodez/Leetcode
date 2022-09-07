9552a463594a233dc435001d75595627b2cbc264
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-09-07 15:28:48 +0530

    Time: 337 ms (21.12%), Space: 20.4 MB (82.70%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
@@ -1,36 +1,48 @@
 class Solution:
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        m=len(matrix)
-        n=len(matrix[0])
-        if target<matrix[0][0] or target>matrix[m-1][n-1]:
-            return False
-        t1,t2=0,0
-        for i in range(max(m,n)):
-            if i<n:
-                if matrix[0][i]<=target:
-                    b=i
-                else:
-                    t1=1
-            if i<m:
-                if matrix[i][0]<=target:
-                    a=i
-                else:
-                    t2=1
-            if t1>0 and t2>0:
-                break
-        for i in range(a+1):
-            l=matrix[i]
-            low=0
-            mid=0
-            high=b
+    # def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+#         m=len(matrix)
+#         n=len(matrix[0])
+#         if target<matrix[0][0] or target>matrix[m-1][n-1]:
+#             return False
+#         t1,t2=0,0
+#         for i in range(max(m,n)):
+#             if i<n:
+#                 if matrix[0][i]<=target:
+#                     b=i
+#                 else:
+#                     t1=1
+#             if i<m:
+#                 if matrix[i][0]<=target:
+#                     a=i
+#                 else:
+#                     t2=1
+#             if t1>0 and t2>0:
+#                 break
+#         for i in range(a+1):
+#             l=matrix[i]
+#             low=0
+#             mid=0
+#             high=b
 
-            while low<=high:
-                mid=(high+low)//2
-                if l[mid]<target:
-                    low=mid+1
-                elif l[mid]>target:
-                    high=mid-1
-                else:
-                    return True
-        else:
-            return False
+#             while low<=high:
+#                 mid=(high+low)//2
+#                 if l[mid]<target:
+#                     low=mid+1
+#                 elif l[mid]>target:
+#                     high=mid-1
+#                 else:
+#                     return True
+#         else:
+#             return False
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        
+        r,c = 0,len(matrix[0])-1
+        while r>=0 and r<len(matrix) and c>=0 and c<len(matrix[0]):
+            if matrix[r][c]==target:
+                return True
+            elif matrix[r][c]>target:
+                c-=1
+            else:
+                r+=1
+        return False
+    
