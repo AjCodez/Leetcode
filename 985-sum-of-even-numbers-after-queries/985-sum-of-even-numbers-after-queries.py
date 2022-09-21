20eb8d2ab29e07a8d37d641d79381780fa576f9a
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-09-21 10:27:22 +0530

    Time: 1302 ms (9.18%), Space: 20.6 MB (43.88%) - LeetHub

---
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.py
@@ -0,0 +1,15 @@
+class Solution:
+    def sumEvenAfterQueries(self, nums: List[int], queries: List[List[int]]) -> List[int]:
+        sums = 0
+        l=[]
+        for i in nums:
+            if i%2==0:
+                sums+=i
+        for i in queries:
+            if nums[i[1]]%2==0:
+                sums-=nums[i[1]]
+            nums[i[1]]+=i[0]
+            if nums[i[1]]%2==0:
+                sums+=nums[i[1]]
+            l.append(sums)
+        return l
