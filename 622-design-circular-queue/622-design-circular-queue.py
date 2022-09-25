61f42a36710ac47b9fc60751d1852e6af195ad0f
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-09-26 00:49:36 +0530

    Time: 140 ms (27.25%), Space: 14.6 MB (21.40%) - LeetHub

--- 622-design-circular-queue/622-design-circular-queue.py
+++ 622-design-circular-queue/622-design-circular-queue.py
@@ -2,38 +2,39 @@ class MyCircularQueue:
 
     def __init__(self, k: int):
         self.k=k
-        self.q=[]
-        self.size=0
+        self.q=[0]*k
+        self.s=0
+        self.f=0
 
     def enQueue(self, value: int) -> bool:
-        if self.size<self.k:
-            self.q.append(value)
-            self.size+=1
-            return True
-        return False
+        if self.s==self.k:
+            return False
+        self.q[(self.f+self.s)%self.k]=value
+        self.s+=1
+        return True
 
     def deQueue(self) -> bool:
-        if self.size!=0:
-            self.q.pop(0)
-            self.size-=1
-            return True
-        return False
+        if self.s==0:
+            return False
+        self.f=(self.f+1)%self.k
+        self.s-=1
+        return True
             
     def Front(self) -> int:
-        if self.q:
-            return self.q[0]
+        if self.s:
+            return self.q[self.f]
         return -1
 
     def Rear(self) -> int:
-        if self.q:
-            return self.q[-1]
+        if self.s:
+            return self.q[(self.f+self.s-1)%self.k]
         return -1
 
     def isEmpty(self) -> bool:
-        return self.size==0
+        return self.s==0
 
     def isFull(self) -> bool:
-        return self.size==self.k
+        return self.s==self.k
 
 
 # Your MyCircularQueue object will be instantiated and called as such:
