eeb8cf87a61aba03c66f98dd36725713cc594e33
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-10-27 05:34:33 +0530

    Time: 276 ms (21.43%), Space: 117.4 MB (16.23%) - LeetHub

---
+++ 0079-word-search/0079-word-search.java
@@ -0,0 +1,40 @@
+class Solution {
+
+    private static boolean bfs(char[][] board, String word, int cr, int cc, int idx) {
+		if (idx == word.length()) {
+			return true;
+		}
+
+		if (cr < 0 || cc < 0 || cr >= board.length || cc >= board[0].length || board[cr][cc] != word.charAt(idx)) {
+			return false;
+
+		}
+
+		board[cr][cc] = '*';
+		int[] r = { -1, 1, 0, 0 };
+		int[] c = { 0, 0, -1, 1 };
+		for (int i = 0; i < c.length; i++) {
+			boolean ans = bfs(board, word, cr + r[i], cc + c[i], idx + 1);
+			if(ans==true) {
+				return ans;
+			}
+
+		}
+		board[cr][cc] =word.charAt(idx);
+
+		return false;
+	}
+
+    public boolean exist(char[][] board, String word) {
+        for (int i = 0; i<board.length; i++){
+            for (int j = 0; j<board[0].length;j++){
+                if(board[i][j]==word.charAt(0)){
+                    boolean ans =bfs(board,word,i,j,0);
+                    if (ans )
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
+}
