bd4a253e5e6e9e69e63f04621e7c9fa6e164fdee
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-10-29 01:24:17 +0530

    Added solution - LeetHub

---
+++ Phone directory - GFG/phone-directory.py
@@ -0,0 +1,39 @@
+#User function Template for python3
+
+class Solution:
+    def displayContacts(self, n, contact, s):
+        l=[]
+        f=False
+        for i in range(len(s)):
+            if not f:
+                ll=set()
+                for j in contact:
+                    if s[:i+1]==j[:i+1]:
+                        ll.add(j)
+                if ll:
+                    l.append(sorted(ll))
+                else:
+                    f=True
+            if f:
+                l.append([0])
+        return l
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        contact = input().split()
+        s = input()
+        
+        ob = Solution()
+        ans = ob.displayContacts(n, contact, s)
+        for i in range(len(s)):
+            for val in ans[i]:
+                print(val, end = " ")
+            print()
+# } Driver Code Ends
