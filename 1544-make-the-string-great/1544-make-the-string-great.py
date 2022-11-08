eb98c52117ebb688b67f9f72f153c81ef1e596a5
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-11-09 01:34:36 +0530

    Time: 57 ms (72.79%), Space: 13.9 MB (61.84%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.py
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -1,11 +1,17 @@
 class Solution:
     def makeGood(self, s: str) -> str:
+        
         ss=[]
+        
         for i in s:
+            
             if not ss:
                 ss.append(i)
+                
             elif (i.islower() and ss[-1].isupper() and i.upper()==ss[-1]) or (ss[-1].islower() and i.isupper() and ss[-1].upper()==i):
                 ss.pop()
+                
             else:
                 ss.append(i)
+                
         return "".join(ss)
