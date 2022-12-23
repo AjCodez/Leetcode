a15de66f89373b9346073fa003968bea104af42c
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-12-24 00:01:47 +0530

    Time: 51 ms (20.13%), Space: 55.6 MB (8.93%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -2,6 +2,7 @@ class Solution {
     public int[] nextGreaterElements(int[] nums) {
         Stack<Integer> s = new Stack<>();
         int [] nums2 = new int[nums.length];
+        
         for(int i=2*nums.length-1;i>-1;i--){
             while(true){
                 if(s.isEmpty()){
