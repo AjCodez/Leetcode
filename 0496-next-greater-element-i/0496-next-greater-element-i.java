bab0e19b10d46001618fe24d66c6b79e67f2d05d
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-12-23 23:00:55 +0530

    Time: 3 ms (94.73%), Space: 42.3 MB (87.55%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -3,8 +3,7 @@ class Solution {
         Stack<Integer> s = new Stack<Integer>();
         HashMap<Integer, Integer> nums3 = new HashMap<>();
         for(int i=nums2.length-1;i>-1;i--){
-            if(s.isEmpty()) nums3.put(nums2[i],-1);
-            else{
+            
                 while(true){
                     if(s.isEmpty()){
                         nums3.put(nums2[i],-1);
@@ -16,7 +15,7 @@ class Solution {
                     }
                     else s.pop();
                 }
-            }
+            
             s.push(nums2[i]);
         }
         for(int i=0;i<nums1.length;i++){
