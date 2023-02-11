45e14d750875f824f1578390043ee962f9168608
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2023-02-12 03:03:57 +0530

    Time: 2 ms (99.32%), Space: 42.6 MB (95.43%) - LeetHub

--- 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.java
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.java
@@ -1,48 +1,52 @@
 class Solution {
-        public int[] shortestAlternatingPaths(int n, int[][] red_edges, int[][] blue_edges) {
-        // Two sets one for blu and another for red
-        Set<Integer>[][] graph = new HashSet[2][n];
-        for (int i = 0; i < n; i++) {
-            graph[0][i] = new HashSet<>();
-            graph[1][i] = new HashSet<>();
+    private int[] h, e, ne, v;
+    private int idx;
+    public int[] shortestAlternatingPaths(int n, int[][] redEdges, int[][] blueEdges) {
+        this.h = new int[n];
+        Arrays.fill(h, -1);
+        int m = redEdges.length + blueEdges.length;
+        this.e = new int[m];
+        this.ne = new int[m];
+        this.v = new int[m];
+        this.idx = 0;
+        for (int[] e : redEdges) add(e[0], e[1], 0);
+        for (int[] e : blueEdges) add(e[0], e[1], 1);
+        Queue<int[]> q = new ArrayDeque<>();
+        int[][] vis = new int[n][2];
+        int[] rst = new int[n];
+        Arrays.fill(rst, -1);
+        rst[0] = 0;
+        vis[0][0] = 1;
+        vis[0][1] = 1;
+        for (int i = h[0]; i != -1; i = ne[i]) {
+            int j = e[i];
+            if (vis[j][v[i]] == 1) continue;
+            q.offer(new int[]{j, v[i]});
+            vis[j][v[i]] = 1;
         }
-        // red edges in 0 - col
-        for (int[] re : red_edges) {
-            graph[0][ re[0] ].add(re[1]);
-        }
-        // blu edges in 1 - col
-        for (int[] blu : blue_edges) {
-            graph[1][ blu[0] ].add(blu[1]);
-        }
-        int[][] res = new int[2][n];
-        // Zero edge is always accessible to itself - leave it as 0
-        for (int i = 1; i < n; i++) {
-            res[0][i] = 2 * n;
-            res[1][i] = 2 * n;
-        }
-        // Q entries are vert with a color (up to that point)
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[] {0, 0}); // either with red
-        q.offer(new int[] {0, 1}); // or with blue
+        int step = 1;
         while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            int vert = cur[0];
-            int colr = cur[1];
-            // No need to keep track of level up to now
-            // only need to keep what color - and the length
-            // is automatically derived from previous node
-            for (int nxt : graph[1 - colr][vert]) {
-                if (res[1 - colr][nxt] == 2 * n) {
-                    res[1 - colr][nxt] = 1 + res[colr][vert];
-                    q.offer(new int[] {nxt, 1 - colr});
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                int[] tmp = q.poll();
+                if (rst[tmp[0]] == -1) rst[tmp[0]] = step;
+                for (int j = h[tmp[0]]; j != -1; j = ne[j]) {
+                    int k = e[j];
+                    if (vis[k][v[j]] == 1) continue;
+                    if (v[j] == tmp[1]) continue;
+                    q.offer(new int[]{k, v[j]});
+                    vis[k][v[j]] = 1;
                 }
             }
+            step++;
         }
-        int[] ans = new int[n];
-        for (int i = 0; i < n; i++) {
-            int t = Math.min(res[0][i], res[1][i]);
-            ans[i] = (t == 2 * n) ? -1 : t;
-        }
-        return ans;
+        return rst;
+    }
+
+    private void add(int a, int b, int c) {
+        e[idx] = b;
+        ne[idx] = h[a];
+        v[idx] = c;
+        h[a] = idx++;
     }
 }
