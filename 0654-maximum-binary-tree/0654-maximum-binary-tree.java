1ded0edefc03d9afb9311aeedc35fb82c90ff685
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2023-02-11 20:26:02 +0530

    Time: 3 ms (46.46%), Space: 42.5 MB (55.58%) - LeetHub

--- 0654-maximum-binary-tree/0654-maximum-binary-tree.java
+++ 0654-maximum-binary-tree/0654-maximum-binary-tree.java
@@ -23,31 +23,31 @@ class Solution {
     
     public TreeNode constructMaximumBinaryTree(int[] nums) {
         
-//         if(nums.length == 0) return null;
+        if(nums.length == 0) return null;
         
-//         int mn = nums[0], mi=0;
-//         for(int i=0;i<nums.length;i++){
-//             if(nums[i]>mn){
-//                 mn = nums[i];
-//                 mi = i;
-//             }
-//         }
-//         TreeNode root = new TreeNode(mn);
-//         root.left = constructMaximumBinaryTree(Arrays.copyOfRange(nums,0,mi));
-//         root.right = constructMaximumBinaryTree(Arrays.copyOfRange(nums,mi+1,nums.length));
-//         return root;
-        Deque<TreeNode> stack = new LinkedList<>();
-        for(int i = 0; i < nums.length; i++) {
-            TreeNode curr = new TreeNode(nums[i]);
-            while(!stack.isEmpty() && stack.peek().val < nums[i]) {
-                curr.left = stack.pop();
+        int mn = nums[0], mi=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>mn){
+                mn = nums[i];
+                mi = i;
             }
-            if(!stack.isEmpty()) {
-                stack.peek().right = curr;
-            }
-            stack.push(curr);
         }
+        TreeNode root = new TreeNode(mn);
+        root.left = constructMaximumBinaryTree(Arrays.copyOfRange(nums,0,mi));
+        root.right = constructMaximumBinaryTree(Arrays.copyOfRange(nums,mi+1,nums.length));
+        return root;
+//         Deque<TreeNode> stack = new LinkedList<>();
+//         for(int i = 0; i < nums.length; i++) {
+//             TreeNode curr = new TreeNode(nums[i]);
+//             while(!stack.isEmpty() && stack.peek().val < nums[i]) {
+//                 curr.left = stack.pop();
+//             }
+//             if(!stack.isEmpty()) {
+//                 stack.peek().right = curr;
+//             }
+//             stack.push(curr);
+//         }
         
-        return stack.isEmpty() ? null : stack.removeLast();
+//         return stack.isEmpty() ? null : stack.removeLast();
     }
 }
